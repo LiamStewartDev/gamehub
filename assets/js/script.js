00ffe530a3b392362f5c9a91543371f44f6e09fe
AcedYu <alexyu299@hotmail.com>
2021-04-07 10:45:34 -0700

    completed rendering list of games function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // JS here. Change function names based on common sense naming conventions
 // Grab html variables here
-
+var gameList = $('#gameList');
+var detailScreen = $('#detailScreen');
 // Global variables here
 var rawgKey = 'fa0bb86079354400af9095c66fac353c';
 var ytKey = 'AIzaSyDq8ij9L8lkIiOCCwHMyDrz4-Jf8ljNWVU';
@@ -16,18 +17,15 @@ var queryTypes = {
 // function to create query string
 var getType = (query, type) => {
   var queryString = `${queryTypes[type]}${query}`;
-  console.log(queryString);
   return queryString;
 }
 
 // function to create the genre query string
 var getGenres = (string) => {
   if (!string) {
-    console.log('blank string returned');
     return '';
   }
   var genreQuery = `&genres=${string}`;
-  console.log(genreQuery);
   return genreQuery;
 }
 
@@ -84,38 +82,64 @@ var getPrices = (title) => {
 
 // renderData function
 var renderData = (data) => {
-  console.log(data);
+  // empty out the list
+  // gameList.empty();
+
+  // define our game data array from the api
   var games = data.data.results;
-  // console.log(games);
-  for (var i = 0; i < games.length; i++) {
-    var id = games[i].id;
-    var name = games[i].name;
-    var rating = games[i].metacritic;
-    // console.log(name);
-    // console.log(id);
-    // console.log(rating);
-  }
+  console.log(games);
+
+  // for loop to create our list of games
+  // for (var i = 0; i < games.length; i++) {
+  //   var id = games[i].id;
+  //   var name = games[i].name;
+  //   var li = $(`<li gameid = "${id}">${name}</li>`);
+  //   li.on('click', (event) => {
+  //     var target = $(event.target);
+  //     idparam = target.attr('gameid');
+  //     getDetails(idparam);
+  //   });
+  //   gameList.append(li);
+  // }
 }
 
 var renderDetails = (game) => {
-  // console.log(game);
+  console.log(game);
   var name = game.data.name;
   var description = game.data.description_raw;
   var imageurl = game.data.background_image;
-  // console.log(name);
-  // console.log(description);
-  // console.log(imageurl);
+  var platforms = game.data.platforms; // an array of objects, the name will be under .name, there is also a system requirements object within only the data for PC will be filled in, the rest will be empty objects.
+  var genre = game.data.genres; // an array of objects, the name will be under .name
+  var developers = game.data.developers; // an array of objects, the name will be under .name
+  var publishers = game.data.publishers; // an array of objects, the name will be under .name
+  var releaseDate = moment(game.data.released).format('MMM D, YYYY');
+
+  var score = game.data.metacritic; // This value might not exist for some of the lesser games
+  var rating = game.data.esrb_rating; //The rating is contained in .name This value might be null
+
+  var detailDisplay = $(``);
 }
 
 var renderYoutube = (data) => {
   console.log(data);
+  var videos = data.data.items;
+  for (var i = 0; i < videos.length; i++) {
+    var vid = videos[i].id.videoId;
+    var videourl = `https://www.youtube.com/watch?v=${vid}`;
+    var channel = videos[i].snippet.channelTitle;
+    var title = videos[i].snippet.title;
+    // console.log(title);
+    // console.log(channel);
+    // console.log(videourl);
+  }
 }
 
 var renderPricing = (data) => {
   console.log(data);
 }
-// getDetails(5679);
 
-getGames('skyrim', 'search');
-getYoutube('coding');
-getPrices('tetris');
+
+// getGames('skyrim', 'search');
+getDetails(290476);
+// getYoutube('coding');
+// getPrices('tetris');
